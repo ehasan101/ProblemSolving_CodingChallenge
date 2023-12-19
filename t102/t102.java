d9ddd50e4a59a32ce29f1e31ab8f3fe156a6a383
Emamul Hasan <74730558+tmb1n@users.noreply.github.com>
2023-12-19 15:44:44 +0600

    Add files via upload

---
+++ t102/t102.java
@@ -0,0 +1,31 @@
+package prac1;
+
+import java.io.*;
+import java.util.Scanner;
+
+class t102 {
+	
+    public static void main (String[] args) {
+    	
+    	System.out.print("Enter Your Strings: ");
+        Scanner scanner = new Scanner(System.in); 
+        String input = scanner.nextLine();   //input data from user & store on 'input' variable
+        String output = ""; //'output' variable initialize it with 0
+        
+         for (int i = input.length()-1; i >= 0; i--) {
+        	 output = output + input.charAt(i); //store new data in 'output' variable
+      } 
+/*
+1. for loop ➔  To get the characters of the input in the reverse order.
+2. input.length()-1 ➔ Because characters in the string are indexed from 0.
+3. input.charAt(i) ➔  Returns character value to the index of the string and store it in 'output'
+*/
+        
+         System.out.println("Your Reversed Strings is: "+output); //finally result print from 'output' variable
+         
+    }
+}
+    
+
+
+
