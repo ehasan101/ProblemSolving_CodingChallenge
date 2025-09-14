2f7609d076c78aa71b6e850f57e64c1a705baccb
h@san <bdemamul.01@gmail.com>
2025-09-14 22:47:29 +0600

    np

---
+++ pyRanProg/pyRanPrac/del1.py
@@ -0,0 +1,17 @@
+price = 900
+
+def test1():
+    print("price in 1st function :", price)  
+
+def test2():
+    print("price in 2nd function :", price)  
+
+
+test1()
+test2()
+
+
+#del price
+
+print(test1())
+print(test2())
